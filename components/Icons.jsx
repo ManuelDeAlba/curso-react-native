523a8ecbda9e346b4dd4f858ef67b22875efc330
Manuel De Alba <ferman5903@gmail.com>
2024-07-18 14:55:23 -0600

    Add expo vector icons

---
+++ components/Icons.jsx
@@ -0,0 +1,15 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export const CircleInfoIcon = props => (
+    <FontAwesome6 name="circle-info" size={24} color="white" {...props} />
+)
+
+export const HomeIcon = props => (
+    <FontAwesome name="home" size={24} color="white" {...props} />
+)
+
+export const ArrowBackCircle = props => (
+    <Ionicons name="arrow-back-circle" size={24} color="white" {...props} />
+)
